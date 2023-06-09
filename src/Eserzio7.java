97a53cc83bcd018006ddf15e1356defbf0a41cff
Salvatore <salvodanna@yahoo.it>
2023-06-09 18:01:58 +0200

    Esercizio svolto

---
+++ src/Eserzio7.java
@@ -0,0 +1,9 @@
+public class Eserzio7 {
+    public static void main(String[] args) {
+        String myname = "Salvatore";
+        if(myname.length() % 2==0) {System.out.println("The number of letters is even");
+        } else {
+            System.out.println("The number of letters is odd");
+        }
+    }
+}
